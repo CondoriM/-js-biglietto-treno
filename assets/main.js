9836f010d15f880f773e226ae3d47e594815b2d9
CondoriM <condoria.miguel@gmail.com>
2022-03-24 15:25:53 +0100

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+//chilometri che vuole percorrere
+const km_user = prompt('Distanza che si desidera percorrere?')
+console.log(km_user);
+
+//eta persona
+const age_user = prompt('inserisca la sua età?')
+console.log(age_user);
